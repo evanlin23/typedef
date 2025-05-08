87a1c0639456227f91f68fd7d19b922949858e0c
evanlin23 <evanlin366@gmail.com>
2025-05-08 17:24:44 -0500

    using tailwind v4

--- src/components/TestResults.tsx
+++ src/components/TestResults.tsx
@@ -25,7 +25,7 @@ const TestResults: React.FC = memo(() => {
   } = state.currentTestStats;
   
   return (
-    <div className="w-3/5 mx-auto bg-gray-800 p-8 rounded-lg mb-8 text-center animate-fadeIn">
+    <div className="w-3/5 mx-auto bg-gray-800 p-8 rounded-lg mb-8 text-center animate-[fadeIn_0.3s_ease-in_forwards]">
       <h3 className="text-2xl mb-5 text-green-400">Test Completed!</h3>
       
       <div className="bg-gray-900 p-4 rounded mb-6 inline-block min-w-52">
