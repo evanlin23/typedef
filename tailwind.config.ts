8ffd287699889a38f1e3c343d051eded37f0a910
evanlin23 <evanlin366@gmail.com>
2025-05-08 09:49:48 -0500

    undo darkgray

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,7 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: [
+    "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
@@ -22,9 +23,6 @@ export default {
         },
       },
     },
-    colors: {
-      darkgray: '#101828'
-    }
   },
   plugins: [],
 } satisfies Config;
